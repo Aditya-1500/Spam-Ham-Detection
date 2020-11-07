a87afb00ef346313b13bbf40259cf0ec5e70bc4e
Aditya Manchanda <adityamanchanda15@gmail.com>
2020-11-07 23:21:02 +0530

    Added models and registered to admin

--- SMSSpamHam/classify/admin.py
+++ SMSSpamHam/classify/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SpamURLS, HamSpamTweets, Words_Users
 
 # Register your models here.
+admin.site.register(SpamURLS)
+admin.site.register(HamSpamTweets)
+admin.site.register(Words_Users)
--- SMSSpamHam/classify/models.py
+++ SMSSpamHam/classify/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class SpamURLS(models.Model):
+    spam_url = models.URLField(unique=True)
+
+class HamSpamTweets(models.Model):
+    tweet = models.TextField()
+    label = models.CharField(max_length=15)
+
+class Words_Users(models.Model):
+    spammy_words = models.CharField(max_length=256)
+    spammy_users = models.CharField(max_length=256)
