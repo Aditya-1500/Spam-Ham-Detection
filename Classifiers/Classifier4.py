6a6186b7836843c474bec3b183a89a44275344ab
Aditya Manchanda <adityamanchanda15@gmail.com>
2020-11-24 23:14:41 +0530

    Updated classifiers

--- Classifiers/Classifier4.py
+++ Classifiers/Classifier4.py
@@ -8,34 +8,50 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 
-def text_process(mess):
-    """
-    Takes in a string of text, then performs the following:
-    1. Remove all punctuation
-    2. Remove all stopwords
-    3. Returns a list of the cleaned text
-    """
-    # Check characters to see if they are in punctuation
-    nopunc = [char for char in mess if char not in string.punctuation]
-
-    # Join the characters again to form the string.
-    nopunc = ''.join(nopunc)
+class MLModel():
+    def __init__(self,tweets):
+        self.tweets = tweets
+        self.pipeline = Pipeline([
+        ('bow', CountVectorizer(analyzer=self.text_process)),  # strings to token integer counts
+        ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
+        ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
+        ])
     
-    # Next just remove any stopwords
-    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+        
+    def text_process(self,mess):
+        """
+        Takes in a string of text, then performs the following:
+        1. Remove all punctuation
+        2. Remove all stopwords
+        3. Returns a list of the cleaned text
+        """
+        # Check characters to see if they are in punctuation
+        nopunc = [char for char in mess if char not in string.punctuation]
+    
+        # Join the characters again to form the string.
+        nopunc = ''.join(nopunc)
+        
+        # Next just remove any stopwords
+        return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
-messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',names=["label", "message"])
-messages['length'] = messages['message'].apply(len)
+    def train_model(self):
+        self.pipeline.fit(self.tweets['message'],self.tweets['label'])
+    
+    def predict(self,tweet):
+        prediction = self.pipeline.predict([tweet])
+        return prediction
 
-msg_train, msg_test, label_train, label_test = \
-train_test_split(messages['message'], messages['label'], test_size=0.2)
 
-pipeline = Pipeline([
-    ('bow', CountVectorizer(analyzer=text_process)),  # strings to token integer counts
-    ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
-    ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
-])
+# messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',names=["label", "message"])
 
-pipeline.fit(msg_train,label_train)
-predictions = pipeline.predict(msg_test)
-print(classification_report(predictions,label_test))
+
+# msg_train, msg_test, label_train, label_test = \
+# train_test_split(messages['message'], messages['label'], test_size=0.2)
+
+# tweets = pd.read_csv('HamSpamTweets.csv')
+# clf = MLModel(tweets)
+# clf.train_model()
+# print(clf.predict(msg_test.iloc[0])," - ",label_test.iloc[0])
+# print(clf.predict(msg_test.iloc[1])," - ",label_test.iloc[1])
+# print(clf.predict(msg_test.iloc[2])," - ",label_test.iloc[2])
+
---
+++ datagen.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 14:11:45 2020
+
+@author: Aditya
+"""
+
+import pandas as pd
+from faker import Faker
+
+fakegen = Faker()
+
+spam_urls = {'url':[]}
+for i in range(10000):
+    spam_urls['url'].append(fakegen.url())
+    
+messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',names=["label", "message"])
+hsClusters = {'Ham':[],'Spam':[]}
+
+spam_words = []
+
+for m in messages[messages['label']=='ham']['message']:
+    hsClusters['Ham'].append(m)
+    if len(hsClusters['Ham']) == 700:
+        break
+
+for m in messages[messages['label']=='spam']['message']:
+    hsClusters['Spam'].append(m)
+    for w in m.split(" "):
+        if w not in spam_words and len(w) > 1:
+            spam_words.append(w)
+    if len(hsClusters['Spam']) == 700:
+        break
+            
+
+spam_users = []
+for i in range(len(spam_words)):
+    spam_users.append(fakegen.name())
+
+spammy_words_users = {'spam_words':spam_words,'spam_users':spam_users}
+
+Spam_urls = pd.DataFrame(spam_urls)
+HSClusters = pd.DataFrame(hsClusters)
+SpammyWordsUsers = pd.DataFrame(spammy_words_users)
+
+Spam_urls.to_csv('SpamUrls.csv',index=False)
+HSClusters.to_csv('Ham_Spam_Clusters.csv',index=False)
+SpammyWordsUsers.to_csv('SpammyWordsUsers.csv',index=False)
+
+messages.to_csv('HamSpamTweets.csv')
+
+    
