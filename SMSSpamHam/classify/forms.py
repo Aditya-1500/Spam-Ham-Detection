b07fa319d117f1693c56a2b62086f8279c4a43d2
Aditya Manchanda <adityamanchanda15@gmail.com>
2020-12-19 23:50:03 +0530

    Update templates + forms in classify app

--- SMSSpamHam/classify/forms.py
+++ SMSSpamHam/classify/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
+def validate_file_extension(value):
+        if not value.name.endswith('.csv'):
+            raise forms.ValidationError("Only CSV file is accepted")
+
 class getFile(forms.Form):
-    file = forms.FileField(required=False)
+    file = forms.FileField(required=False,validators=[validate_file_extension],widget=forms.FileInput(attrs={'accept':'.csv'}))
 
 class getMessage(forms.Form):
-    message = forms.CharField(widget=forms.Textarea,required=False)
+    message = forms.CharField(widget=forms.Textarea(
+                                    attrs={"placeholder":"Username\nTweet"}),
+                    required=False)
