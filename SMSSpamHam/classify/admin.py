e3d4cdb4ef1745bbaefae4e072e8b224f8c221d2
Aditya Manchanda <adityamanchanda15@gmail.com>
2020-12-23 20:22:20 +0530

    Removed unnecessary models

--- SMSSpamHam/classify/admin.py
+++ SMSSpamHam/classify/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import SpamURLS, HamSpamTweets, Words_Users
 
 # Register your models here.
-admin.site.register(SpamURLS)
-admin.site.register(HamSpamTweets)
-admin.site.register(Words_Users)
--- SMSSpamHam/classify/models.py
+++ SMSSpamHam/classify/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class SpamURLS(models.Model):
-    spam_url = models.URLField(unique=True)
-
-class HamSpamTweets(models.Model):
-    tweet = models.TextField()
-    label = models.CharField(max_length=15)
-
-class Words_Users(models.Model):
-    spammy_words = models.CharField(max_length=256)
-    spammy_users = models.CharField(max_length=256)
